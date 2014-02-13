d460184287a277aa587c278fc196a4fc0b827465
gd <gregdzezinski@gmail.com>
2014-02-12 23:52:04 -0500

    init

---
+++ LearnAzureServiceBusSender/Publisher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus;
+using Microsoft.WindowsAzure;
+
+namespace LearnAzureServiceBusSender
+{
+    public class Publisher
+    {
+        public void Create()
+        {
+            var connString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(connString);
+
+            if (!namespaceManager.TopicExists("Topic1"))
+            {
+                namespaceManager.CreateTopic("Topic1");
+            }
+        }
+    }
+}
