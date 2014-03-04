43c9a6bf687019985d1b1a7c0c19c1de6050d997
gd <gregdzezinski@gmail.com>
2014-03-03 23:18:04 -0500

    added web project

---
+++ LearnAzureServiceBusSender/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearnAzureServiceBusSender
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
